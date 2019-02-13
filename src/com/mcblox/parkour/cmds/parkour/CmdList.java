1ffea7598d7998e5fd287aba125dc0be100c147a
BuildTools <unconfigured@null.spigotmc.org>
2019-02-13 18:03:20 -0500

    Commit

--- src/com/mcblox/parkour/cmds/parkour/CmdList.java
+++ src/com/mcblox/parkour/cmds/parkour/CmdList.java
@@ -1,16 +1,29 @@
 package com.mcblox.parkour.cmds.parkour;
 
+import static org.bukkit.ChatColor.*;
+
 import java.util.List;
 
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import com.mcblox.parkour.objects.BloxCommand;
+import com.mcblox.parkour.objects.Course;
 
 public class CmdList extends BloxCommand {
 
 	@Override
 	public void execute(CommandSender sender, String[] args) {
+		
+		// Initialize variables
+		Player player = (Player) sender;
+		List<Course> courses = Course.courses;
+		
+		// List courses
+		for (Course course : courses) {
+			player.sendMessage(GRAY + "" + course.getId() + RED + ": " + GRAY + course.getName());
+		}
+		
 	}
 
 	@Override
