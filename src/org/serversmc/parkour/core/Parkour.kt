3a17ff563ef0457e8f2f2c3809efe17f105341e6
Dennis Lysenko <dennislysenko@hotmail.com>
2020-02-22 00:37:27 -0500

    Registered events

--- src/org/serversmc/parkour/core/Parkour.kt
+++ src/org/serversmc/parkour/core/Parkour.kt
@@ -1,7 +1,10 @@
 package org.serversmc.parkour.core
 
+import org.bukkit.*
 import org.bukkit.plugin.java.JavaPlugin
 import org.serversmc.ServersMC
+import org.serversmc.parkour.commands.*
+import org.serversmc.parkour.events.*
 import org.serversmc.utils.Console
 
 class Main : JavaPlugin() {
@@ -17,7 +20,14 @@ class Main : JavaPlugin() {
         // Create Data folder
         if (!dataFolder.exists()) dataFolder.mkdirs()
         // Register Events
-        
+        Bukkit.getPluginManager().apply {
+	        registerEvents(ParkourPlay, Parkour)
+	        registerEvents(CmdSetStart, Parkour)
+	        registerEvents(CmdRegion, Parkour)
+	        registerEvents(CmdSetFinish, Parkour)
+	        registerEvents(CmdDelete, Parkour)
+	        registerEvents(CmdAddCheck, Parkour)
+        }
         // Load Courses
         
         // Check for Update
