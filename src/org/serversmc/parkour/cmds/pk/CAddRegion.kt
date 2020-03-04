f5d88c82b813087a2869c96e66c0307b2a397e55
Dennis Lysenko <dennislysenko@hotmail.com>
2020-03-04 11:05:15 -0500

    Created CAddRegion.kt command

--- src/org/serversmc/parkour/cmds/pk/CAddRegion.kt
+++ src/org/serversmc/parkour/cmds/pk/CAddRegion.kt
@@ -1,9 +1,13 @@
 package org.serversmc.parkour.cmds.pk
 
+import org.bukkit.*
 import org.bukkit.command.*
 import org.bukkit.entity.*
+import org.bukkit.event.*
+import org.bukkit.event.block.*
 import org.bukkit.permissions.*
 import org.serversmc.parkour.cmds.*
+import org.serversmc.parkour.core.*
 import org.serversmc.parkour.enums.*
 import org.serversmc.parkour.interfaces.*
 import org.serversmc.parkour.objects.*
@@ -24,6 +28,15 @@ object CAddRegion : ICommand {
 		}
 		// Check if player is already in session
 		if (session.contains(player)) {
+			// Check if subcommand test was called
+			if (args.isNotEmpty() && args[0].equals("test", true)) {
+				player.sendMessage("${AQUA}Testing region...")
+				session[player]!!.hide()
+				Bukkit.getScheduler().scheduleSyncDelayedTask(PLUGIN, {
+					session[player]!!.show()
+				}, 20L)
+				return
+			}
 			// Cancel session
 			player.sendMessage("${AQUA}Finished region editor")
 			session.remove(player)
@@ -36,6 +49,22 @@ object CAddRegion : ICommand {
 		player.sendMessage("${GRAY}${getUsage()}${AQUA}will save and close the editor")
 		// Add to session and create region
 		session[player] = course.createRegion()
+	}
+	
+	@EventHandler
+	fun onBlockBreak(event: BlockBreakEvent) {
+		// Initialize player
+		val player = event.player
+		// Check if player is in session
+		if (!session.contains(player)) return
+	}
+	
+	@EventHandler
+	fun onBlockPlace(event: BlockPlaceEvent) {
+		// Initialize player
+		val player = event.player
+		// Check if player is in session
+		if (!session.contains(player)) return
 	}
 	
 	override fun tabComplete(player: Player, args: MutableList<out String>): MutableList<String>? = ArrayList()
