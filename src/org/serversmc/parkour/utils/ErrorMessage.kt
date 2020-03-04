5b7327d11169eaf0de987cffa68ed19dd84cf3cf
Dennis Lysenko <dennislysenko@hotmail.com>
2020-03-03 19:32:40 -0500

    Added Documentation

--- src/org/serversmc/parkour/utils/ErrorMessage.kt
+++ src/org/serversmc/parkour/utils/ErrorMessage.kt
@@ -6,14 +6,24 @@ import org.serversmc.parkour.interfaces.*
 
 object ErrorMessage {
 	
+	/**
+	 * Please enter a number
+	 */
 	fun enterNumber(sender: CommandSender) {
 		sender.sendMessage("${RED}Please enter a number!")
 	}
 	
+	/**
+	 * Course id $id not found!
+	 */
 	fun courseIdNotFound(sender: CommandSender, id: Int) {
 		sender.sendMessage("${RED}Course id '$GRAY$id$RED' not found!")
 	}
 	
+	/**
+	 * No course selected. Select a course using '/pk select'
+	 * or using 'cmd.getUsage()'
+	 */
 	fun noCourseSelect(sender: CommandSender, cmd: ICommand) {
 		sender.sendMessage("${RED}No course selected. Select a course using '$GRAY/pk select$RED'")
 		sender.sendMessage("${RED}or using '$GRAY${cmd.getUsage()}$RED'")
